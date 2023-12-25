9652379d53653df277dec8c5d80c3ed28d4497f6
Paweł <automationpawel23@onet.pl>
2023-12-25 12:47:44 +0100

    25.12.2023

--- src/test/java/exceptions/ExceptionsCheck.java
+++ src/test/java/exceptions/ExceptionsCheck.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class ExceptionsCheck {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvalidAgeException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj wiek");
         int age = scanner.nextInt();
         if(age<0){
-            throw  new InvalidAgeException("Your age is not valid")
+            throw new InvalidAgeException("Your age is not valid");
         }
         if(age>=18){
             System.out.println("Jestes pelnoletni");
